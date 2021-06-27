0704e84458437b4609533ab8c965672fa28424d2
LUCIFER-dev-king <nihaljesiya@gmail.com>
2021-06-27 23:10:58 +0530

    add modal

--- src/pages/core/Base.js
+++ src/pages/core/Base.js
@@ -1,12 +1,6 @@
 import React from "react";
 import Header from "./Header";
-import {
-  FaAddressCard,
-  FaHome,
-  FaNotesMedical,
-  FaPage4,
-  FaPiggyBank,
-} from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 import SideBar from "./SideBar";
 const Base = ({ children }) => {
   return (
@@ -18,6 +12,52 @@ const Base = ({ children }) => {
         <div className='col-md-9'>
           <Header></Header>
           <div className='bg-dark text-white p-4'>{children}</div>
+
+          <button
+            type='button'
+            className='position-absolute bottom-0 end-0 m-3 btn btn-danger rounded'
+            data-bs-toggle='modal'
+            data-bs-target='#testModal'
+          >
+            <FaTimes />
+          </button>
+
+          <div
+            class='modal fade'
+            id='testModal'
+            tabindex='-1'
+            aria-labelledby='testModalLabel'
+            aria-hidden='true'
+          >
+            <div class='modal-dialog modal-dialog-centered'>
+              <div class='modal-content'>
+                <div class='modal-header'>
+                  <h5 class='modal-title' id='testModalLabel'>
+                    Modal title
+                  </h5>
+                  <button
+                    type='button'
+                    class='btn-close'
+                    data-bs-dismiss='modal'
+                    aria-label='Close'
+                  ></button>
+                </div>
+                <div class='modal-body'>...</div>
+                <div class='modal-footer'>
+                  <button
+                    type='button'
+                    class='btn btn-secondary'
+                    data-bs-dismiss='modal'
+                  >
+                    Close
+                  </button>
+                  <button type='button' class='btn btn-primary'>
+                    Save changes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
