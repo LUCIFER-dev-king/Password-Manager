2b0690c75f62b5c8aefb0c8a68c4761a741f54ea
LUCIFER-dev-king <nihaljesiya@gmail.com>
2022-01-29 22:39:21 +0530

    bug fix

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,10 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import SignIn from "./auth/SignIn";
 import SignUp from "./auth/SignUp";
-import Address from "./pages/address/Address";
 import BankAccount from "./pages/bankAccount/BankAccount";
 import Home from "./pages/home/Home";
 import Notes from "./pages/notes/Notes";
 import Password from "./pages/password/Password";
-import PaymentCards from "./pages/paymentCards/PaymentCards";
 import PrivateRoute from "./auth/PrivateRoute";
 import { Provider } from "react-redux";
 import store from "./redux/store";
@@ -20,11 +18,9 @@ export default function Routes() {
           <PrivateRoute path="/" exact component={Home} />
           <Route path="/signin" exact component={SignIn} />
           <Route path="/signup" exact component={SignUp} />
-          <Route path="/password" exact component={Password} />
-          <Route path="/notes" exact component={Notes} />
-          <Route path="/address" exact component={Address} />
-          <Route path="/paymentcards" exact component={PaymentCards} />
-          <Route path="/bankaccount" exact component={BankAccount} />
+          <PrivateRoute path="/password" exact component={Password} />
+          <PrivateRoute path="/notes" exact component={Notes} />
+          <PrivateRoute path="/bankaccount" exact component={BankAccount} />
         </Switch>
       </BrowserRouter>
     </Provider>
